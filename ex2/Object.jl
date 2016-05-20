f3c4e0e26c895bf69f546ec5e0189fdf8796a97c
Sebastian Lindner <sebastian.lindner@tuhh.de>
2016-05-20 15:40:36 +0200

    rotateObject()

--- ex2/Object.jl
+++ ex2/Object.jl
@@ -4,3 +4,26 @@ type Object
  Object(x::Vector{Vec4f}) = new(x)
  Object(x...) = new(collect(Vec4f,x))
 end
+
+function render(object; figAxis=[-1,1,-1,1])
+	axis(figAxis)
+	x = []
+	y = []
+	for i in 1:length(object.vertices)
+		x = [x; object.vertices[i].e1]
+		y = [y; object.vertices[i].e2]
+	end
+	x = [x; object.vertices[1].e1]
+	y = [y; object.vertices[1].e2]
+	plot(x, y, linestyle="-")
+end
+
+function rotateObject(O)
+  for i = 1:60
+		render(rotz((2*pi) + (2 * i * (pi / 60))) * houseOfSantaClaus, figAxis=[-2,2,-2,2])
+    sleep(0.001)
+    if i < 60
+      clf()
+    end
+	end
+end
--- ex2/ex2.jl
+++ ex2/ex2.jl
@@ -22,37 +22,27 @@ M2 = Mat4f(
 M1 * M2
 
 
-# function render(object;figAxis=[-1,1,-1,1])
-# 	axis(figAxis)
-# 	x = []
-# 	y = []
-# 	for i in 1:length(object.vertices)
-# 		x = [x; object.vertices[i].e1]
-# 		y = [y; object.vertices[i].e2]
-# 	end
-# 	x = [x; object.vertices[1].e1]
-# 	y = [y; object.vertices[1].e2]
-# 	plot(x, y, linestyle="-")
-# end
-#
-# houseOfSantaClaus = Object(
-# 	Vec4f(-1,-1,0,1),
-# 	Vec4f(1,-1,0,1),
-# 	Vec4f(-1,1,0,1),
-# 	Vec4f(0,2,0,1),
-# 	Vec4f(1,1,0,1),
-# 	Vec4f(-1,-1,0,1),
-# 	Vec4f(-1,1,0,1),
-# 	Vec4f(1,1,0,1),
-# 	Vec4f(1,-1,0,1)
-# )
-#
-# T = Transformation(
-# 	Vec4f(2, 0, 0, 0),
-# 	Vec4f(0, 2, 0, 0),
-# 	Vec4f(0, 0, 2, 0),
-# 	Vec4f(0, 0, 0, 2)
-# )
-#
+
+
+houseOfSantaClaus = Object(
+	Vec4f(-1,-1,0,1),
+	Vec4f(1,-1,0,1),
+	Vec4f(-1,1,0,1),
+	Vec4f(0,2,0,1),
+	Vec4f(1,1,0,1),
+	Vec4f(-1,-1,0,1),
+	Vec4f(-1,1,0,1),
+	Vec4f(1,1,0,1),
+	Vec4f(1,-1,0,1)
+)
+
+T = Transformation(
+	Vec4f(2, 0, 0, 0),
+	Vec4f(0, 2, 0, 0),
+	Vec4f(0, 0, 2, 0),
+	Vec4f(0, 0, 0, 2)
+)
+
+rotateObject(houseOfSantaClaus)
 # render(translation(1, 0, 0) * houseOfSantaClaus, figAxis=[-2,2,-2,2])
 # render(rotx(pi/4) * houseOfSantaClaus, figAxis=[-2,2,-2,2])
